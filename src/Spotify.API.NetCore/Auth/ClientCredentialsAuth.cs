ebf28a4886da2a13dbd3a816dc9e5497f24fdc1c
Barış Ceviz <me@barisceviz.com>
2018-06-24 01:33:21 +0300

    Added Auth

---
+++ src/Spotify.API.NetCore/Auth/ClientCredentialsAuth.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Spotify.API.NetCore.Enums;
+using Spotify.API.NetCore.Models;
+
+namespace Spotify.API.NetCore.Auth
+{
+    public class ClientCredentialsAuth
+    {
+        public Scope Scope { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+
+        /// <summary>
+        ///     Starts the auth process and
+        /// </summary>
+        /// <returns>A new Token</returns>
+        public Token DoAuth()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("Authorization",
+                    "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(ClientId + ":" + ClientSecret)));
+
+                NameValueCollection col = new NameValueCollection
+                {
+                    {"grant_type", "client_credentials"},
+                    {"scope", Scope.GetStringAttribute(" ")}
+                };
+
+                byte[] data;
+                try
+                {
+                    data = wc.UploadValues("https://accounts.spotify.com/api/token", "POST", col);
+                }
+                catch (WebException e)
+                {
+                    using (StreamReader reader = new StreamReader(e.Response.GetResponseStream()))
+                    {
+                        data = Encoding.UTF8.GetBytes(reader.ReadToEnd());
+                    }
+                }
+                return JsonConvert.DeserializeObject<Token>(Encoding.UTF8.GetString(data));
+            }
+        }
+
+        /// <summary>
+        ///     Starts the auth process async and
+        /// </summary>
+        /// <returns>A new Token</returns>
+        public async Task<Token> DoAuthAsync()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("Authorization",
+                    "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(ClientId + ":" + ClientSecret)));
+
+                NameValueCollection col = new NameValueCollection
+                {
+                    {"grant_type", "client_credentials"},
+                    {"scope", Scope.GetStringAttribute(" ")}
+                };
+
+                byte[] data;
+                try
+                {
+                    data = await wc.UploadValuesTaskAsync("https://accounts.spotify.com/api/token", "POST", col);
+                }
+                catch (WebException e)
+                {
+                    using (StreamReader reader = new StreamReader(e.Response.GetResponseStream()))
+                    {
+                        data = Encoding.UTF8.GetBytes(await reader.ReadToEndAsync());
+                    }
+                }
+                return JsonConvert.DeserializeObject<Token>(Encoding.UTF8.GetString(data));
+            }
+        }
+    }
+}
