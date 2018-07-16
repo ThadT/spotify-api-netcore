2eedf3105c92f13e18c8a5d502aa08c7bb712fee
Barış Ceviz <me@barisceviz.com>
2018-07-16 18:20:31 +0300

    Added Uri type

---
+++ src/Spotify.API.NetCore/Enums/UriType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spotify.API.NetCore.Enums
+{
+    public enum UriType
+    {
+        none,
+        track,
+        album,
+        artist,
+        playlist,
+        user
+    }
+}
