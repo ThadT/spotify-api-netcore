eac464f26a5f2f1f112a76790c49a7c382498ed0
Barış Ceviz <me@barisceviz.com>
2018-07-16 18:21:55 +0300

    Added Util tests

---
+++ tests/SpotifyAPI.NetCore.Tests/UtilTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Spotify.API.NetCore;
+using Xunit;
+
+namespace SpotifyAPI.NetCore.Tests
+{
+    public class UtilTest
+    {
+        [Fact]
+        public void TimestampShouldBeNoFloatingPoint()
+        {
+            string timestamp = DateTime.Now.ToUnixTimeMillisecondsPoly().ToString();
+
+            timestamp.Should().NotContain(".");
+            timestamp.Should().NotContain(",");
+        }
+    }
+}
